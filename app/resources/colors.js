50de275788638093a57700ceb44c2066018e6aa2
Amena Khatun <amena@bitmascot.com>
2022-04-28 09:29:39 +0600

    Goal 1: Added Splash Screen for both Android & iOS

---
+++ app/resources/colors.js
@@ -0,0 +1,31 @@
+const Colors = {
+    black: "#000000",
+    white: "#FFFFFF",
+    green: "#008000",
+    red: "#FF0000",
+    yellow: "#FFFF00",
+    blue: "#F000FF",
+    orange: '#CC4A31',
+
+    main: "#232323",
+    primary: "#22B0AF",
+    secondary: "#1BB55C",
+    error: "#EB5757",
+
+    inputLabel: "#212121",
+    inputText: "#000000",
+    inputBorder: "#a6a6a6",
+    inputBorder2: "#e3e3e3",
+
+    checkUnchecked: "#232323",
+
+    radioUnchecked: "#232323",
+
+    buttonRed: "#FF9595",
+
+    toastSuccess: "#1BB55C",
+    toastError: "#EB5757",
+    toastWarning: "#FFBB12"
+}
+
+export default Colors
